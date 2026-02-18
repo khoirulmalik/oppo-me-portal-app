0cdc1c5c865603b77e20025b78b13427ea59f2b7
Khoirul Malik <khoirulmalik03@gmail.com>
2026-02-18 14:24:18 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
